f62bc6028bf96986ee368adf27a5f8df36876bf0
evangelikapirade <100015297+evangelikapirade@users.noreply.github.com>
2022-04-21 01:59:52 +0800

    Create main.java

---
+++ main.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args){
+        Anjing anjing1 = new Anjing();
+        Ras rasanjing = new Ras();
+        Hewan hewann = new Hewan();
+
+        anjing1.namahewan();
+        rasanjing.Rass();
+        hewann.bunyi_Hewan();
+       
+    }
+}
